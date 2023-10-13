916f6cbade8e64c4b06087f9f8a66e200438abb8
Nisar <145307487+nisarusan@users.noreply.github.com>
2023-10-13 05:04:46 +0200

    Revert "Added componenten in React"
    This reverts commit 1c85304f90f21edb3eeb42ddf901ff4cb8d22b2b.

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,11 @@
 import './App.css'
-import citroenen from './assets/citroenen.jpeg';
-import limoenen from './assets/ijsblokjes.jpg';
-import ijsblokjes from './assets/limoenen.png';
-import {ReactComponent as ShoppingCart} from "./assets/winkelmandje.svg";
-import Product from "./components/Product.jsx";
 
 function App() {
-
-    //Hoeft niet in React een Reference point, alleen bij javascript normaal
-    // const buttonReference = document.getElementById('button');
-    // buttonReference.addEventListener('click', logClick);
-    function logClick() {
-        console.log("You clicked");
-    }
-
     return (
-        <>
-            <nav>
-                <ul>
-                    <li><a href="/">Shop</a></li>
-                    <li><a href="#">Ons verhaal</a></li>
-                    <li><a href="#">Blog</a></li>
-                </ul>
-                <ShoppingCart className="shopping-cart-icon"/>
-            </nav>
-            <header><h1>
-                Fruit perfection
-            </h1>
-                <button type="button" onClick={logClick}>Shop nu</button>
-            </header>
-            <main>
-                <Product image={citroenen} title="Citroen"
-                         description="Een citroen is voor de meeste mensen te zuur om zo uit de hand te eten. Van citroen kun je het vruchtvlees, het sap en de schil gebruiken. Het sappige, lichtgele zure vruchtvlees versterkt de smaak van ander voedsel."
-                />
-                <Product image={limoenen} title="Limoenen"
-                         description="Een citroen is voor de meeste mensen te zuur om zo uit de hand te eten. Van citroen kun je het vruchtvlees, het sap en de schil gebruiken. Het sappige, lichtgele zure vruchtvlees versterkt de smaak van ander voedsel."/>
-                <Product image={ijsblokjes} title="ijsblokjes"
-                         description="Een ijsblokje voor de meeste mensen te zuur om zo uit de hand te eten. Van citroen kun je het vruchtvlees, het sap en de schil gebruiken. Het sappige, lichtgele zure vruchtvlees versterkt de smaak van ander voedsel."/>
-            </main>
-        </>
-    );
-
+        <main>
+            Begin hier met de tutorial!
+        </main>
+    )
 }
 
 export default App
